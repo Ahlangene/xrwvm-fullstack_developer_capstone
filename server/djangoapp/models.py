a47b139f7e13f957a37d2e2bd358baee864efe1e
Ahlangene <ndana.ahlangene@gmail.com>
2026-07-17 05:05:33 -0400

    Database rectified

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,43 +1,25 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# 1. Car Make Model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# 2. Car Model Model
 class CarModel(models.Model):
-    #  dealer_id = models.IntegerField()
-    make_id=models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    models_name = models.CharField(max_length=100)
-    model_type = models.CharField(max_length=10, choices=[("Sedan","SEDAN"), ("SUV","SUV"), ("Wagon","WAGON")])
+    # Changed from make_id to car_make for standard Django relational style
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=[("Sedan","SEDAN"), ("SUV","SUV"), ("Wagon","WAGON")])
     year = models.IntegerField(default=2023, validators=[
         MaxValueValidator(2023),
         MinValueValidator(2015)
     ])
 
     def __str__(self):
-        return self.model_name    
+        return self.name # FIXED: Matches the exact field name above now   
